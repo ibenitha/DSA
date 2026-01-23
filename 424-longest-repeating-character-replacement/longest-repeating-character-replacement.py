8bc1f55864fa07e7f5c115264cf00c24d759d65e
ibenitha <99011687+ibenitha@users.noreply.github.com>
2026-01-23 12:52:56 +0200

    Time: 85 ms (83.32%) | Memory: 19.8 MB (16.45%) - LeetSync

--- 424-longest-repeating-character-replacement/longest-repeating-character-replacement.py
+++ 424-longest-repeating-character-replacement/longest-repeating-character-replacement.py
@@ -3,8 +3,11 @@ class Solution:
         l, r, most_freq_char, res, freq = 0,0,0,0, defaultdict(int)
         while r < len(s):
             freq [s[r]] += 1
+        
             most_freq_char = max(most_freq_char, freq[s[r]])
+
             while (r-l +1) - most_freq_char > k:
+                
                 freq[s[l]] -= 1
                 l += 1 
             res = max(res,r -l +1)
