74060aa933cc86526ca7ac368dd393f79b859a9c
ibenitha <99011687+ibenitha@users.noreply.github.com>
2026-01-25 16:05:29 +0200

    Time: 4 ms (80.27%) | Memory: 23.4 MB (18.20%) - LeetSync

---
+++ 229-majority-element-ii/majority-element-ii.py
@@ -0,0 +1,10 @@
+class Solution:
+    def majorityElement(self, nums: List[int]) -> List[int]:
+        counts = Counter(nums)
+        n = len(nums)
+        ele = []
+        for num, count in counts.items():
+            if count > n//3:
+               ele.append(num)
+        return ele
+        
